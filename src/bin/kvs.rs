c07efbbb6b4643b480d7c6cd057842dee11cae95
root1iu <root1iu@aliyun.com>
2021-08-05 13:49:08 +0800

    accept command line arguments

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,56 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+use kvs::KvStore;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("kvs")
+        .arg(Arg::with_name("version").short("V").long("version"))
+        .subcommands(vec![
+            SubCommand::with_name("get")
+                .about("get the value of key")
+                .arg(Arg::with_name("key").index(1).required(true)),
+            SubCommand::with_name("rm")
+                .about("rm the key")
+                .arg(Arg::with_name("key").index(1).required(true)),
+            SubCommand::with_name("set")
+                .about("set a key-value pair")
+                .args(&[
+                    Arg::with_name("key").index(1).required(true),
+                    Arg::with_name("value").index(2).required(true),
+                ]),
+        ])
+        .get_matches();
+
+    if matches.is_present("version") {
+        println!("0.1.0");
+        return;
+    }
+
+    let kv = KvStore::new();
+    match matches.subcommand_name() {
+        Some("get") => {
+            if let Some(get_matches) = matches.subcommand_matches("get") {
+                let key = String::from(get_matches.value_of("key").unwrap());
+                kv.get(key);
+            }
+        }
+        Some("rm") => {
+            if let Some(rm_matches) = matches.subcommand_matches("rm") {
+                let key = String::from(rm_matches.value_of("key").unwrap());
+                kv.remove(key);
+            }
+        }
+        Some("set") => {
+            if let Some(set_matches) = matches.subcommand_matches("set") {
+                let key = String::from(set_matches.value_of("key").unwrap());
+                let value = String::from(set_matches.value_of("value").unwrap());
+                kv.set(key, value);
+            }
+        }
+        None => {
+            panic!("unimplemented");
+        }
+        _ => unreachable!(),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,16 @@ impl KvStore {
         KvStore {}
     }
 
-    pub fn set(&self, k: String, v: String) {}
+    pub fn set(&self, k: String, v: String) {
+        panic!("unimplemented");
+    }
 
-    pub fn remove(&self, k: String) {}
+    pub fn remove(&self, k: String) {
+        panic!("unimplemented");
+    }
 
     pub fn get(&self, k: String) -> Option<String> {
+        panic!("unimplemented");
         Some(String::from(""))
     }
 }
