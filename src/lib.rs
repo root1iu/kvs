bb158f328133d6e7ab7531ccdea1805f0e07099b
root1iu <root1iu@aliyun.com>
2021-08-04 18:46:16 -0700

    make the tests compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
 // empty
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&self, k: String, v: String) {}
+
+    pub fn remove(&self, k: String) {}
+
+    pub fn get(&self, k: String) -> Option<String> {
+        Some(String::from(""))
+    }
+}
