784a9f1290e5be05e38c7de11d7a27ee3071dfa6
root1iu <root1iu@aliyun.com>
2021-08-09 09:36:13 +0800

    use structopt for parsing command line arguments

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,65 +1,51 @@
-extern crate clap;
+extern crate structopt;
 
-use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "kvs", about = "an in-memory key/value store")]
+struct Opt {
+    #[structopt(name = "version", long = "version", short = "V")]
+    version: bool,
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    #[structopt(name = "get")]
+    Get { key: String },
+    #[structopt(name = "rm")]
+    Rm { key: String },
+    #[structopt(name = "set")]
+    Set { key: String, value: String },
+}
 
 fn main() {
-    let matches = App::new("kvs")
-        .arg(Arg::with_name("version").short("V").long("version"))
-        .subcommands(vec![
-            SubCommand::with_name("get")
-                .about("get the value of key")
-                .author(env!("CARGO_PKG_AUTHORS"))
-                .version(env!("CARGO_PKG_VERSION"))
-                .arg(Arg::with_name("key").index(1).required(true)),
-            SubCommand::with_name("rm")
-                .about("rm the key")
-                .author(env!("CARGO_PKG_AUTHORS"))
-                .version(env!("CARGO_PKG_VERSION"))
-                .arg(Arg::with_name("key").index(1).required(true)),
-            SubCommand::with_name("set")
-                .about("set a key-value pair")
-                .author(env!("CARGO_PKG_AUTHORS"))
-                .version(env!("CARGO_PKG_VERSION"))
-                .args(&[
-                    Arg::with_name("key").index(1).required(true),
-                    Arg::with_name("value").index(2).required(true),
-                ]),
-        ])
-        .get_matches();
+    let opt = Opt::from_args();
 
-    if matches.is_present("version") {
+    if opt.version {
         println!(env!("CARGO_PKG_VERSION"));
         return;
     }
 
     let mut kv = KvStore::new();
-    match matches.subcommand_name() {
-        Some("get") => {
-            if let Some(get_matches) = matches.subcommand_matches("get") {
-                let key = String::from(get_matches.value_of("key").unwrap());
-                kv.get(key);
-            }
+    match opt.cmd {
+        Some(Command::Get { key }) => {
+            kv.get(key);
             panic!("unimplemented");
         }
-        Some("rm") => {
-            if let Some(rm_matches) = matches.subcommand_matches("rm") {
-                let key = String::from(rm_matches.value_of("key").unwrap());
-                kv.remove(key);
-            }
+        Some(Command::Rm { key }) => {
+            kv.remove(key);
             panic!("unimplemented");
         }
-        Some("set") => {
-            if let Some(set_matches) = matches.subcommand_matches("set") {
-                let key = String::from(set_matches.value_of("key").unwrap());
-                let value = String::from(set_matches.value_of("value").unwrap());
-                kv.set(key, value);
-            }
+        Some(Command::Set { key, value }) => {
+            kv.set(key, value);
             panic!("unimplemented");
         }
         None => {
             panic!("unimplemented");
         }
-        _ => unreachable!(),
     }
 }
